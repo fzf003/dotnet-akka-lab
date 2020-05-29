6bcaac5ff41a1a79b5f2077b44a3521dea716c42
fzf003 <280086260@qq.com>
2020-05-29 17:44:18 +0800

    删除注释

--- Src/DotnetDocker/AkkaTcpClient/Program.cs
+++ Src/DotnetDocker/AkkaTcpClient/Program.cs
@@ -25,26 +25,7 @@ namespace AkkaTcpClient
 
 
 
-           /* using (var system = ActorSystem.Create("echo-client-system"))
-            {
-                var port = 9001;
-
-                
-
-                var actor = system.ActorOf(Props.Create(() => new ClientService(IPAddress.Loopback, port)), "echo-client");
-                        
-                for (; ; )
-                {
-                    system.Scheduler.Advanced.ScheduleRepeatedly(TimeSpan.Zero, TimeSpan.FromMilliseconds(1000), () => {
-                        actor.Tell(new SendMessage(Guid.NewGuid().ToString("N")));
-                    });
-                    
-                    Console.ReadKey();
-                    // actor.Tell(StartMessage.Instance);
-                    //actor.Tell(StopMessage.Instance);
-                }
-
-            }*/
+         
         }
     }
 }
